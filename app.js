a57b6ba7d9e5f2ab1642dc14224d12d18d179626
wmitchel <williamjamesmitchel@gmail.com>
2016-04-17 22:49:26 -0400

    Post only tweets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var Twitter = require('twitter');
+var express = require('express');
 
 var client = new Twitter({
   consumer_key: '5BD4KFCSUq9yWrEzokqchzGIC',
@@ -7,12 +8,43 @@ var client = new Twitter({
   access_token_secret: '3y9H8jdwGg5XdJcxfwbojQIoQ06pMUXfSmUdBputvc2oM'
 });
 
-client.post('statuses/update', {status: 'No I am not.'}, function(error, tweet, response){
-  if (!error) {
-    console.log("Success!");
-  }
-});
+var visitCount = 0;
 
-client.get('search/tweets', {q: 'csc400Node'}, function(error, tweets, response){
-   console.log(tweets);
-});
+
+// client.get('search/tweets', {q: 'csc400Node'}, function(error, tweets, response){
+//    console.log(tweets);
+// });
+var app= express();
+// allTweets = [];
+
+app.get('/', function (req, res) {
+    visitCount += 1;
+    var toPost = "Visits since server restart: " + visitCount + ".";
+    client.post('statuses/update', {status: toPost}, function(error, tweet, response){
+          if (!error) {
+            console.log("Success!");
+        } else {
+            console.log(error);
+        }
+    });
+
+    // client.get('statuses/user_timeline', {screen_name: 'csc400Node'}, function(error, tweets, response){
+    //    //console.log(tweets);
+    //    if (error) {
+    //        console.log(error);
+    //        console.log("There was an error");
+    //    } else {
+    //        console.log("success?");
+    //         allTweets[0] = 5;
+    //        //console.log(tweets);
+    //        console.log(" ");
+    //    }
+    // });
+    //
+    // console.log(allTweets + "alltweets");
+    // console.log("All Tweets");
+
+    res.render('index.ejs', { count: visitCount });
+})
+
+app.listen(3000)
